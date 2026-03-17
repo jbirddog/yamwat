e3006e4f885a137378c1b5a0f450c3704e318f0e
Jon Herron <jon.herron@yahoo.com>
2026-03-17 07:50:24 -0400

    wip

--- docker.mk
+++ docker.mk
@@ -5,13 +5,13 @@ RUN_ARGS := --rm -u $(ME) -v .:/src $(IMG)
 RUN := docker run $(RUN_ARGS)
 RUN_IT := docker run -it $(RUN_ARGS)
 
-.PHONY: img run sh
+.PHONY: ci img sh
+
+ci:
+	$(RUN)
 
 img:
 	docker build -t $(IMG) .
-
-run:
-	$(RUN)
 
 sh:
 	$(RUN_IT) /bin/bash
