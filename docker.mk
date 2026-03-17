71c7efd90196e7e44d477b2e145a326edf3a4b26
Jon Herron <jon.herron@yahoo.com>
2026-03-17 07:48:32 -0400

    wip

--- docker.mk
+++ docker.mk
@@ -1,7 +1,17 @@
-.PHONY: img run
+ME ?= $(shell id -u):$(shell id -g)
+
+IMG := yamwat
+RUN_ARGS := --rm -u $(ME) -v .:/src $(IMG)
+RUN := docker run $(RUN_ARGS)
+RUN_IT := docker run -it $(RUN_ARGS)
+
+.PHONY: img run sh
 
 img:
-	docker build -t yamwat .
+	docker build -t $(IMG) .
 
 run:
-	docker run --rm -v .:/src yamwat
+	$(RUN)
+
+sh:
+	$(RUN_IT) /bin/bash
